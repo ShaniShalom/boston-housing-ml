296effc50c786e54428513b1376ad93e86d3977a
SHANI SHALOM <SHANITI023@GMAIL.COM>
2026-03-22 09:57:16 -0400

    Update model_trainer.py

--- model_trainer.py
+++ model_trainer.py
@@ -8,18 +8,19 @@ def train_housing_model(df):
     Trains a simple Linear Regression model.
     Predicts 'median_house_value' based on 'median_income'.
     """
-    # X = המידע המשפיע (הכנסה), y = המטרה (מחיר)
+    
+    # X = the influencing information (income), y = the target (price)
     X = df[['median_income']] 
     y = df['median_house_value']
     
-    # חלוקה ל-80% למידה ו-20% מבחן
+    # Divided into 80% learning and 20% testingן
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    # יצירת המודל ואימונו
+    # Model creation and training
     model = LinearRegression()
     model.fit(X_train, y_train)
     
-    # בדיקת ביצועים על נתוני המבחן
+    # Performance testing on test data
     predictions = model.predict(X_test)
     rmse = np.sqrt(mean_squared_error(y_test, predictions))
     r2 = r2_score(y_test, predictions)
